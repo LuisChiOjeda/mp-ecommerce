75979f5b82c14fa5d3fde7a27d8efb0dac846c22
Luis Chi Ojeda <chiojeda.luis@gmail.com>
2021-02-27 13:27:54 -0600

    regreso a commit 1 e implementacion 2

--- app.js
+++ app.js
@@ -1,6 +1,55 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var port = process.env.PORT || 3000
+const mercadopago = require("mercadopago");
+var port = process.env.PORT || 3000;
+
+//Handle call to backend and generate preference.
+document.getElementById("checkout-btn").addEventListener("click", function() {
+
+    $('#checkout-btn').attr("disabled", true);
+    
+    var orderData = {
+      quantity: document.getElementById("quantity").value,
+      description: document.getElementById("product-description").innerHTML,
+      price: document.getElementById("unit-price").innerHTML
+    };
+      
+    fetch("/create_preference", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                
+            },
+            body: JSON.stringify(orderData),
+      })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(preference) {
+            createCheckoutButton(preference.id);
+            $(".shopping-cart").fadeOut(500);
+            setTimeout(() => {
+                $(".container_payment").show(500).fadeIn();
+            }, 500);
+        })
+        .catch(function() {
+            alert("Unexpected error");
+            $('#checkout-btn').attr("disabled", false);
+        });
+  });
+  
+  //Create preference when click on checkout button
+  function createCheckoutButton(preference) {
+    var script = document.createElement("script");
+    
+    // The source domain must be completed according to the site for which you are integrating.
+    // For example: for Argentina ".com.ar" or for Brazil ".com.br".
+    script.src = "https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js";
+    script.type = "text/javascript";
+    script.dataset.preferenceId = preference;
+    document.getElementById("button-checkout").innerHTML = "";
+    document.querySelector("#button-checkout").appendChild(script);
+  }
 
 var app = express();
  
@@ -11,12 +60,45 @@ app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
 
+mercadopago.configurations.setAccessToken("APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181");
+
 app.get('/', function (req, res) {
     res.render('home');
 });
 
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
+});
+app.post("/create_preference", (req, res) => {
+
+	let preference = {
+		items: [{
+			title: req.body.description,
+			unit_price: Number(req.body.price),
+			quantity: Number(req.body.quantity),
+		}],
+		back_urls: {
+			"success": "http://"+port+"/home",
+			"failure": "http://"+port,
+			"pending": "http://"+port
+		},
+		auto_return: 'approved',
+	};
+
+	mercadopago.preferences.create(preference)
+		.then(function (response) {
+			res.json({id :response.body.id})
+		}).catch(function (error) {
+			console.log(error);
+		});
+});
+
+app.get('/feedback', function(request, response) {
+	 response.json({
+		Payment: request.query.payment_id,
+		Status: request.query.status,
+		MerchantOrder: request.query.merchant_order_id
+	})
 });
 
 app.listen(port);
